da7a7e118528acd4d1d70770d9bb50c6655741b8
Aygun <aygunaydin@gmail.com>
2016-08-11 16:09:09 +0300

    index is updated

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <html>
 <body bgcolor="#E6E6FA" >
-
-<a class="twitter-timeline" href="https://twitter.com/aygayd">Tweets by aygayd</a>
+<center>
+<a class="twitter-timeline" width="400" href="https://twitter.com/aygayd">Tweets by aygayd</a>
 <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-
+<center>
 </body>
 </html>
